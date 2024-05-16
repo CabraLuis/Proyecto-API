2ddcebd2b9cd7508bd7d374f047f45545905031d
Luis Cabra <zetinaluis03@gmail.com>
2024-05-15 19:58:22 -0500

    basic prisma crud

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,5 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
-import formidable from "formidable";
 
 const app = express();
 const prisma = new PrismaClient();
@@ -8,31 +7,59 @@ const prisma = new PrismaClient();
 app.use(express.json());
 
 app.get("/games", async (req, res, next) => {
-  const allGames = await prisma.game.findMany();
-  res.send(allGames);
+  const getGames = await prisma.game.findMany();
+  res.send(getGames).status(200);
 });
 
-app.delete("/games", (req, res, next) => {
-  res.send("si").status(200);
-});
-
-app.post("/games", (req, res, next) => {
-  const form = formidable({
-    uploadDir: "uploads",
-    keepExtensions: true,
+app.get("/games/:id", async (req, res, next) => {
+  const getGame = await prisma.game.findUnique({
+    where: { id: req.params.id },
   });
-
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      next(err);
-      return;
-    }
-    res.json({ fields, files });
-  });
+  res.send(getGame).status(200);
 });
 
-app.put("/games", (req, res, next) => {
-  res.send("si").status(200);
+app.delete("/games/:id", async (req, res, next) => {
+  const deleteGame = await prisma.game.delete({
+    where: { id: req.params.id },
+  });
+  res.send(deleteGame).status(200);
+});
+
+app.post("/games", async (req, res, next) => {
+  const createUser = await prisma.game.create({
+    data: {
+      title: "title",
+      developer: "dev",
+      distributor: "d",
+      genre: "g",
+      released: "r",
+    },
+  });
+  res.send().status(200);
+});
+
+app.patch("/games/:id", async (req, res, next) => {
+  const updateGame = await prisma.game.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      title: "title",
+      developer: "dev",
+      distributor: "d",
+      genre: "g",
+      released: "r",
+    },
+  });
+  res.send(updateGame).status(200);
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("URL not defined");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: err.message });
 });
 
 app.listen(3000, (req, res) => {
