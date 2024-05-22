489b6d71b1f9806f218ce2ab35dbc26f76309f2c
CabraLuis <zetinaluis03@gmail.com>
2024-05-22 07:40:13 -0500

    sheesh

--- src/index.js
+++ src/index.js
@@ -1,16 +1,114 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+import { SwaggerTheme, SwaggerThemeNameEnum } from "swagger-themes";
 
 const app = express();
 const prisma = new PrismaClient();
 
+const swaggerJSDocOptions = {
+  definition: {
+    openapi: "3.0.3",
+    info: {
+      title: "Games API",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+    tags: [
+      {
+        name: "games",
+        description: "Basic info of your games",
+      },
+    ],
+    components: {
+      schemas: {
+        Game: {
+          required: ["title", "developer", "distributor", "genre", "released"],
+          type: "object",
+          properties: {
+            title: {
+              type: "string",
+              example: "Galaga",
+            },
+            developer: {
+              type: "string",
+              example: "Namco",
+            },
+            distributor: {
+              type: "string",
+              example: "Namco",
+            },
+            genre: {
+              type: "string",
+              example: "Fixed Shooter",
+            },
+            released: {
+              type: "string",
+              example: "1981-09-25",
+            },
+          },
+        },
+      },
+    },
+  },
+  apis: ["./src/index.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerJSDocOptions);
+const theme = new SwaggerTheme();
+const options = {
+  customCss: theme.getBuffer(SwaggerThemeNameEnum.FEELING_BLUE),
+};
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, options));
+app.use(express.static("public"));
+app.use("/api-docs-json", (req, res) => {
+  res.json(swaggerSpec);
+});
+
 app.use(express.json());
 
+/**
+ * @openapi
+ * /games:
+ *  get:
+ *   tags:
+ *    - games
+ *   summary: Find all games
+ *   description: Returns all games
+ *   responses:
+ *    200:
+ *     description: Returns a JSON containing all registered games
+ */
 app.get("/games", async (req, res, next) => {
   const getGames = await prisma.game.findMany();
   res.send(getGames).status(200);
 });
 
+/**
+ * @openapi
+ * /games/{gameID}:
+ *  get:
+ *   tags:
+ *    - games
+ *   summary: Find a game by ID
+ *   description: Returns a single game
+ *   parameters:
+ *    - name: gameID
+ *      in: path
+ *      description: ID of game to return
+ *      required: true
+ *      schema:
+ *       type: integer
+ *       format: int64
+ *   responses:
+ *    200:
+ *     description: Returns a JSON object of the game
+ */
 app.get("/games/:id", async (req, res, next) => {
   const getGame = await prisma.game.findUnique({
     where: { id: parseInt(req.params.id) },
@@ -18,6 +116,26 @@ app.get("/games/:id", async (req, res, next) => {
   res.send(getGame).status(200);
 });
 
+/**
+ * @openapi
+ * /games/{gameID}:
+ *  delete:
+ *   tags:
+ *    - games
+ *   summary: Delete a game by ID
+ *   description: Deletes a  game
+ *   parameters:
+ *    - name: gameID
+ *      in: path
+ *      description: ID of game to delete
+ *      required: true
+ *      schema:
+ *       type: integer
+ *       format: int64
+ *   responses:
+ *    200:
+ *     description: Returns a JSON object of the deleted game
+ */
 app.delete("/games/:id", async (req, res, next) => {
   const deleteGame = await prisma.game.delete({
     where: { id: parseInt(req.params.id) },
@@ -25,6 +143,24 @@ app.delete("/games/:id", async (req, res, next) => {
   res.send(deleteGame).status(200);
 });
 
+/**
+ * @openapi
+ * /games/{gameID}:
+ *  post:
+ *   tags:
+ *    - games
+ *   summary: Add a game
+ *   description: Add a new game to the database
+ *   requestBody:
+ *    description: Create a new game
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/components/schemas/Game'
+ *   responses:
+ *    200:
+ *     description: Returns a JSON object of the game
+ */
 app.post("/games", async (req, res, next) => {
   const createUser = await prisma.game.create({
     data: {
@@ -38,6 +174,32 @@ app.post("/games", async (req, res, next) => {
   res.send().status(200);
 });
 
+/**
+ * @openapi
+ * /games/{gameID}:
+ *  patch:
+ *   tags:
+ *    - games
+ *   summary: Modify a game
+ *   description: Modify a game in the database
+ *   parameters:
+ *    - name: gameID
+ *      in: path
+ *      description: ID of game to modify
+ *      required: true
+ *      schema:
+ *       type: integer
+ *       format: int64
+ *   requestBody:
+ *    description: Modify a game
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/components/schemas/Game'
+ *   responses:
+ *    200:
+ *     description: Returns a JSON object of the game
+ */
 app.patch("/games/:id", async (req, res, next) => {
   const updateGame = await prisma.game.update({
     where: {
